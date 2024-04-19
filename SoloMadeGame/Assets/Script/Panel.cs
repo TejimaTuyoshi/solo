a7738e5d37e3495112129f4b1922fd4254dfc7b7
tejimatsuyoshi <tejima.tuyoshi@icloud.com>
2024-04-19 12:17:51 +0900

    ゲームオーバー画面を編集+タイトルに戻れるように修正

--- SoloMadeGame/Assets/Script/Panel.cs
+++ SoloMadeGame/Assets/Script/Panel.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Panel : MonoBehaviour
 {
@@ -14,5 +15,10 @@ public class Panel : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void Title()
+    {
+        SceneManager.LoadScene("stage1", LoadSceneMode.Single);
     }
 }
